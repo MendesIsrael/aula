2e1c37acc1fff133081ca5d57fb4043521098b36
Israel Mendes da Silva <israel.mendes97@hotmail.com>
2023-01-24 20:21:10 -0300

    estrutura

---
+++ 001/src/pages/styles/global.ts
@@ -0,0 +1,10 @@
+import { globalCss } from ".";
+
+export const globalStyles = globalCss({
+  '*': {
+    boxSizing : 'border-box',
+    margin    : 0,
+    padding   : 0,
+    fontFamily: 'sans-serif',
+  },
+})
